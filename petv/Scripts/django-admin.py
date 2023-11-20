95b23c0f27a90e9c98bf22de55efa426539505c3
Egor Semenov <Hinodem@mail.ru>
2023-11-20 18:03:56 +0300

    add catalog_page_for_sql

--- petv/Scripts/django-admin.py
+++ petv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Dev\pet_spacesite\pet_venv\Scripts\python.exe
+#!C:\Dev\pet_spacesite\petv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
---
+++ plans/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views 
+
+#нужно добавить еще страницу с детальным представлением интернета
+
+app_name = 'plans'
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='plans/plans.html'), name='all_plans')
+]
