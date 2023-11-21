11c43f9fa25ae09cfe1b1c32cc790a9f3811b7a5
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2023-11-21 22:12:37 +0300

    intermidate com.

--- plans/views.py
+++ plans/views.py
@@ -5,7 +5,10 @@ from .models import PlansTitleSpeed
 def index(request):
     template_name = 'plans/plans.html'
 
-    plan_list = PlansTitleSpeed.objects.values('id', 'title', 'price', 'option', 'equipment', 'period', 'speed')
+    plan_list = PlansTitleSpeed.objects.values(
+        'id', 'title', 'price', 'option', 'equipment',
+        'period', 'speed'
+        ).filter(is_on_main=True)
 
     context = {
         'plan_list': plan_list
