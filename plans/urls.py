bdcdcda0d36c79645597f7ed833c1070e05f7810
Egor Semenov <Hinodem@mail.ru>
2023-11-22 17:07:41 +0300

    add detail

--- plans/urls.py
+++ plans/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from . import views 
-
-#нужно добавить еще страницу с детальным представлением интернета
+from . import views
 
 app_name = 'plans'
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='plans/plans.html'), name='all_plans')
-    path('', views.index, name='all_plans')
+    path('', views.index, name='all_plans'),
+    path('<int:pk>/', views.detail, name='plan_detail')
 ]
