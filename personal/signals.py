a391ad29e73392af41881a5942d4e96e4a5a0ab0
Egor Semenov <Hinodem@mail.ru>
2024-01-12 17:08:28 +0300

    Добавлен сигнал, при регистрации - пользователю присваивается группа. Группы нет - создается.

---
+++ personal/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import models, get_user_model
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def add_user_to_human_group(sender, instance, created, **kwargs):
+    if created:
+        # Получаем или создаем группу "human"
+        group, _ = models.Group.objects.get_or_create(name='human')
+        # Добавить пользователя в группу
+        instance.groups.add(group)
