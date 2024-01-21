b0ce4e05920256a57bc7d1b6ff14e29601be0d14
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-01-21 22:49:51 +0300

    Добавлен профиль пользователя

--- html_css/plans.js
+++ html_css/plans.js
@@ -1,9 +1,19 @@
-$(document).ready(function(){
-    $('.plans-box-pricing').slick({
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true
-    });
-    
+const offersContainer = document.querySelector('.profile-offers');
+const offers = document.querySelectorAll('.profile-offer');
+let currentIndex = 0;
+
+function updateDisplay() {
+    offersContainer.scrollLeft = currentIndex * offers[0].offsetWidth;
+}
+
+document.getElementById('prevButton').addEventListener('click', () => {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    updateDisplay();
 });
+
+document.getElementById('nextButton').addEventListener('click', () => {
+    currentIndex = Math.min(currentIndex + 1, offers.length - 2); // Оставляем видимыми два элемента за раз
+    updateDisplay();
+});
+
+updateDisplay();
